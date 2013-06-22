c032a0701abaff7e2dc0033fa4bd0864d7c1e22c
Lin Jen-Shin <godfat@godfat.org>
2013-06-22 23:55:26 +0800

    more tests!

--- test/test_matcher.rb
+++ test/test_matcher.rb
@@ -13,7 +13,6 @@ describe Muack::Matcher do
     should 'match' do
       mock(Str).say(is_a(String)){ |arg| arg.reverse }
       Str.say('Foo').should.eq 'ooF'
-
       Muack.verify.should.eq true
       Muack::EnsureReset.call
     end
@@ -25,6 +24,18 @@ describe Muack::Matcher do
       expected = 'Muack::API.within(0..9)'
       matcher.to_s   .should.eq expected
       matcher.inspect.should.eq expected
+    end
+
+    should 'match' do
+      mock(Str).say(within(0..9)){ |arg| arg*2 }
+      Str.say(5).should.eq 10
+      Muack.verify.should.eq true
+
+      mock(Str).say(within(%[a b])){ |arg| arg.upcase }
+      Str.say('b').should.eq 'B'
+      Muack.verify.should.eq true
+
+      Muack::EnsureReset.call
     end
   end
 end
