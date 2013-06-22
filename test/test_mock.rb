545ab52fe27a052b772a16f98651a316416f1406
Lin Jen-Shin <godfat@godfat.org>
2013-06-22 17:51:47 +0800

    change description

--- test/test_mock.rb
+++ test/test_mock.rb
@@ -7,17 +7,17 @@ describe Muack::Mock do
       Muack.verify.should.eq true
     end
 
-    should 'mock with regular method call' do
+    should 'mock with regular method' do
       moo = mock.moo(true){ 'boo' }
       moo.moo(true).should.eq 'boo'
     end
 
-    should 'accept matcher' do
+    should 'mock with is_a matcher' do
       moo = mock('Moo').say(is_a(String)){ |arg| arg.reverse }
       moo.say('Foo').should.eq 'ooF'
     end
 
-    should 'stub regular method' do
+    should 'stub with regular method' do
       moo = stub.foo{ 'goo' }
       3.times{ moo.foo.should.eq 'goo' }
       Muack.verify.should.eq true
