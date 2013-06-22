5f363b73ac0f48559c518b3addef97ab09ded586
Lin Jen-Shin <godfat@godfat.org>
2013-06-22 22:59:44 +0800

    Muack::Within to_s/inspect test

--- test/test_matcher.rb
+++ test/test_matcher.rb
@@ -2,10 +2,21 @@
 require 'muack/test'
 
 describe Muack::Matcher do
-  should 'have human readable to_s and inspect' do
-    matcher = is_a(String)
-    expected = 'Muack::API.is_a(String)'
-    matcher.to_s   .should.eq expected
-    matcher.inspect.should.eq expected
+  describe Muack::IsA do
+    should 'have human readable to_s and inspect' do
+      matcher = is_a(String)
+      expected = 'Muack::API.is_a(String)'
+      matcher.to_s   .should.eq expected
+      matcher.inspect.should.eq expected
+    end
+  end
+
+  describe Muack::Within do
+    should 'have human readable to_s and inspect' do
+      matcher = within(0..9)
+      expected = 'Muack::API.within(0..9)'
+      matcher.to_s   .should.eq expected
+      matcher.inspect.should.eq expected
+    end
   end
 end
