dc1aa405b19b06a7fc08510ed1910ae943701c36
Lin Jen-Shin <godfat@godfat.org>
2013-06-22 23:48:32 +0800

    make them global for better testing

--- lib/muack/test.rb
+++ lib/muack/test.rb
@@ -5,6 +5,20 @@ require 'muack'
 Bacon.summary_on_exit
 include Muack::API
 
+Obj = Object.new
+Str = 'Moo'
+def Obj.inspect
+  'obj'
+end
+
+Muack::EnsureReset = lambda{
+  [Obj, Str].each do |o|
+    o.methods.select{ |m|
+      m.to_s.start_with?('__muack_mock') || m.to_s.start_with?('say')
+    }.should.empty
+  end
+}
+
 module Kernel
   def eq? rhs
     self == rhs
--- test/test_mock.rb
+++ test/test_mock.rb
@@ -2,63 +2,49 @@
 require 'muack/test'
 
 describe Muack::Mock do
-  obj = Object.new
-  str = 'Moo'
-  def obj.inspect
-    'obj'
-  end
-
-  ensure_reset = lambda{
-    [obj, str].each do |o|
-      o.methods.select{ |m|
-        m.to_s.start_with?('__muack_mock') || m.to_s.start_with?('say')
-      }.should.empty
-    end
-  }
-
   describe 'Muack.verify==true' do
     after do
       Muack.verify.should.eq true
-      ensure_reset.call
+      Muack::EnsureReset.call
     end
 
     should 'mock with regular method' do
-      mock(obj).say(true){ 'boo' }
-      obj.say(true).should.eq 'boo'
+      mock(Obj).say(true){ 'boo' }
+      Obj.say(true).should.eq 'boo'
     end
 
     should 'mock with is_a matcher' do
-      mock(str).say(is_a(String)){ |arg| arg.reverse }
-      str.say('Foo').should.eq 'ooF'
+      mock(Str).say(is_a(String)){ |arg| arg.reverse }
+      Str.say('Foo').should.eq 'ooF'
     end
 
     should 'stub with regular method' do
-      stub(obj).say{ 'goo' }
-      3.times{ obj.say.should.eq 'goo' }
+      stub(Obj).say{ 'goo' }
+      3.times{ Obj.say.should.eq 'goo' }
     end
 
     should 'mock twice' do
-      mock(obj).say(true){ obj.saya }
-      mock(obj).saya{ 'coo' }
-      obj.say(true).should.eq 'coo'
+      mock(Obj).say(true){ Obj.saya }
+      mock(Obj).saya{ 'coo' }
+      Obj.say(true).should.eq 'coo'
     end
 
     should 'mock external object' do
-      mock(str).say{ str.sub('M', 'H') }
-      str.say.should.eq 'Hoo'
+      mock(Str).say{ Str.sub('M', 'H') }
+      Str.say.should.eq 'Hoo'
     end
   end
 
   describe 'Muack.verify==false' do
     after do
       Muack.verify.should.eq false
-      ensure_reset.call
+      Muack::EnsureReset.call
     end
 
     should 'raise Muack::Unexpected error if passing unexpected argument' do
-      mock(obj).say(true){ 'boo' }
+      mock(Obj).say(true){ 'boo' }
       begin
-        obj.say(false)
+        Obj.say(false)
         'never'.should.eq 'reach'
       rescue Muack::Unexpected => e
         e.expected.should.eq 'obj.say(true)'
