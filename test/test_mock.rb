ca4358722d378fd420fe36724ab80a4eb3f49d42
Lin Jen-Shin <godfat@godfat.org>
2013-06-22 17:49:45 +0800

    test cleanup

--- test/test_mock.rb
+++ test/test_mock.rb
@@ -2,55 +2,58 @@
 require 'muack/test'
 
 describe Muack::Mock do
-  before do
-    Muack.reset
-  end
-
-  should 'mock with regular method call' do
-    moo = mock.moo(true){ 'boo' }
-    moo.moo(true).should.eq 'boo'
-    Muack.verify.should.eq true
-  end
-
-  should 'raise Muack::Unexpected error if passing unexpected argument' do
-    moo = mock.moo(true){ 'boo' }
-    obj = moo.__mock_object
-    def obj.inspect
-      'moo'
+  describe 'Muack.verify == true' do
+    after do
+      Muack.verify.should.eq true
     end
-    begin
-      moo.moo(false).should.eq 'boo'
-      'never'.should.eq 'reach'
-    rescue Muack::Unexpected => e
-      e.expected.should.eq 'moo.moo(true)'
-      e.was     .should.eq 'moo.moo(false)'
+
+    should 'mock with regular method call' do
+      moo = mock.moo(true){ 'boo' }
+      moo.moo(true).should.eq 'boo'
+    end
+
+    should 'accept matcher' do
+      moo = mock('Moo').say(is_a(String)){ |arg| arg.reverse }
+      moo.say('Foo').should.eq 'ooF'
+    end
+
+    should 'stub regular method' do
+      moo = stub.foo{ 'goo' }
+      3.times{ moo.foo.should.eq 'goo' }
+      Muack.verify.should.eq true
+    end
+
+    should 'mock chain' do
+      moo = mock
+      moo.moo(true){ moo.boo }.mock.boo{ 'coo' }
+      moo.moo(true).should.eq 'coo'
+    end
+
+    should 'mock external object' do
+      moo = mock('Moo')
+      moo.sleep{ moo.sub('M', 'H') }
+      moo.sleep.should.eq 'Hoo'
+    end
+  end
+
+  describe 'Muack.verify == false' do
+    after do
       Muack.verify.should.eq false
     end
-  end
 
-  should 'mock chain' do
-    moo = mock
-    moo.moo(true){ moo.boo }.mock.boo{ 'coo' }
-    moo.moo(true).should.eq 'coo'
-    Muack.verify.should.eq true
-  end
-
-  should 'mock external object' do
-    moo = mock('Moo')
-    moo.sleep{ moo.sub('M', 'H') }
-    moo.sleep.should.eq 'Hoo'
-    Muack.verify.should.eq true
-  end
-
-  should 'stub regular method' do
-    moo = stub.foo{ 'goo' }
-    3.times{ moo.foo.should.eq 'goo' }
-    Muack.verify.should.eq true
-  end
-
-  should 'accept matcher' do
-    moo = mock('Moo').say(is_a(String)){ |arg| arg.reverse }
-    moo.say('Foo').should.eq 'ooF'
-    Muack.verify.should.eq true
+    should 'raise Muack::Unexpected error if passing unexpected argument' do
+      moo = mock.moo(true){ 'boo' }
+      obj = moo.__mock_object
+      def obj.inspect
+        'moo'
+      end
+      begin
+        moo.moo(false).should.eq 'boo'
+        'never'.should.eq 'reach'
+      rescue Muack::Unexpected => e
+        e.expected.should.eq 'moo.moo(true)'
+        e.was     .should.eq 'moo.moo(false)'
+      end
+    end
   end
 end
