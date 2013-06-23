04925ca0ce7624581808061cd8b57c9f81b1c82a
Lin Jen-Shin <godfat@godfat.org>
2013-06-23 21:48:06 +0800

    quick and dirty proxy implementation

--- lib/muack/proxy.rb
+++ lib/muack/proxy.rb
@@ -5,7 +5,32 @@ module Muack
   class Proxy < Mock
     # used for mocked object to dispatch mocked method
     def __mock_block_call defi, actual_args
-      object.__send__(defi.original_method, *actual_args)
+      if defi.original_method
+        object.__send__(defi.original_method, *actual_args)
+      end
+    end
+
+    def __mock_inject_method defi
+      mock = self # remember the context
+
+      object.singleton_class.module_eval do
+        if instance_methods(false).include?(defi.msg)
+          # store original method
+          original_method = Mock.find_new_name(self, defi.msg).to_sym
+          alias_method original_method, defi.msg
+          defi.original_method = original_method
+        end
+
+        # define mocked method
+        define_method defi.msg do |*actual_args, &actual_block|
+          r = mock.__mock_dispatch(defi.msg, actual_args, actual_block)
+          if defi.original_method
+            r
+          else
+            super(*actual_args, &actual_block)
+          end
+        end
+      end
     end
   end
 end
---
+++ test/test_proxy.rb
@@ -0,0 +1,38 @@
+
+require 'muack/test'
+
+describe Muack::Proxy do
+  describe 'Muack.verify==true' do
+    after do
+      Muack.verify.should.eq true
+      Muack::EnsureReset.call
+    end
+
+    should 'stub with regular method' do
+      proxy(Str).reverse
+      Str.reverse.should.eq 'ooM'
+    end
+  end
+
+  describe 'Muack.verify==false' do
+    after do
+      Muack.reset
+      Muack::EnsureReset.call
+    end
+
+    should 'raise Muack::Expected error if passing unexpected argument' do
+      proxy(Str).reverse
+      Str.reverse.should.eq 'ooM'
+      begin
+        Str.reverse
+        'never'.should.eq 'reach'
+      rescue Muack::Expected => e
+        e.expected      .should.eq '"Moo".reverse()'
+        e.expected_times.should.eq 1
+        e.actual_times  .should.eq 2
+        e.message       .should.eq "\nExpected: \"Moo\".reverse()\n  " \
+                                   "called 1 times\n but was 2 times."
+      end
+    end
+  end
+end
