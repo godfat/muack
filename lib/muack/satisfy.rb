1809dbee438801161a488f847f93e2ea158cf301
Lin Jen-Shin <godfat@godfat.org>
2013-06-23 17:42:46 +0800

    eliminate warnings

--- lib/muack/satisfy.rb
+++ lib/muack/satisfy.rb
@@ -1,6 +1,6 @@
 
 module Muack
-  class Satisfy < Struct.new(:block)
+  class Satisfy < Struct.new(:block, :api_args)
     def match actual_arg
       !!block.call(actual_arg)
     end
@@ -16,43 +16,39 @@ module Muack
     end
 
     def api_args
-      @api_args || [block]
+      super || [block]
     end
   end
 
   class IsA < Satisfy
     def initialize klass
-      @api_args = [klass]
-      super lambda{ |actual_arg| actual_arg.kind_of?(klass) }
+      super lambda{ |actual_arg| actual_arg.kind_of?(klass) }, [klass]
     end
   end
 
   class Anything < Satisfy
     def initialize
-      @api_args = []
-      super lambda{ |_| true }
+      super lambda{ |_| true }, []
     end
   end
 
   class Match < Satisfy
     def initialize regexp
-      @api_args = [regexp]
-      super lambda{ |actual_arg| regexp.match(actual_arg) }
+      super lambda{ |actual_arg| regexp.match(actual_arg) }, [regexp]
     end
   end
 
   class HashIncluding < Satisfy
     def initialize hash
-      @api_args = [hash]
       super lambda{ |actual_arg|
-        actual_arg.values_at(*hash.keys) == hash.values }
+        actual_arg.values_at(*hash.keys) == hash.values }, [hash]
     end
   end
 
   class Within < Satisfy
     def initialize range_or_array
-      @api_args = [range_or_array]
-      super lambda{ |actual_arg| range_or_array.include?(actual_arg) }
+      super lambda{ |actual_arg| range_or_array.include?(actual_arg) },
+            [range_or_array]
     end
   end
 end
