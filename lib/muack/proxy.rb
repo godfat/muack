b74e2a1dd4c40ddb8b43f3bc4c785d8ebf58a0c2
Lin Jen-Shin <godfat@godfat.org>
2013-06-24 19:30:53 +0800

    now we finally don't have to worry about AnyInstanceOf

--- lib/muack/proxy.rb
+++ lib/muack/proxy.rb
@@ -3,53 +3,27 @@ require 'muack/mock'
 
 module Muack
   class Proxy < Mock
-    # used for mocked object to dispatch mocked method
     def __mock_block_call defi, actual_args
-      if defi.original_method && !object.kind_of?(AnyInstanceOf)
-        result = object.__send__(defi.original_method, *actual_args)
-        if defi.block
-          defi.block.call(result)
-        else
-          result
-        end
-      end
+      # handle block call in injected method, since we need to call origin
     end
 
-    def __mock_inject_method defi
+    def __mock_inject_mock_method target, defi
       mock = self # remember the context
+      target.__send__(:define_method, defi.msg){|*actual_args, &actual_block|
+        mock.__mock_dispatch(defi.msg, actual_args, actual_block)
 
-      object.singleton_class.module_eval do
-        if instance_methods(false).include?(defi.msg)
-          # store original method
-          original_method = Mock.find_new_name(self, defi.msg)
-          alias_method original_method, defi.msg
-          defi.original_method = original_method
-        end
-
-        # define mocked method
-        define_method defi.msg do |*actual_args, &actual_block|
-          r = mock.__mock_dispatch(defi.msg, actual_args, actual_block)
-          if defi.original_method
-            if mock.object.kind_of?(AnyInstanceOf)
-              result = __send__(defi.original_method, *actual_args, &actual_block)
-              if defi.block
-                defi.block.call(result)
+        ret = if defi.original_method
+                __send__(defi.original_method, *actual_args, &actual_block)
               else
-                result
+                super(*actual_args, &actual_block)
               end
-            else
-              r
-            end
-          else
-            result = super(*actual_args, &actual_block)
-            if defi.block
-              defi.block.call(result)
-            else
-              result
-            end
-          end
+
+        if defi.block
+          defi.block.call(ret)
+        else
+          ret
         end
-      end
+      }
     end
   end
 end
