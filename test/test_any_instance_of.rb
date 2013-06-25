e6f0c53159f69f92779a8868eed5db80662f77a3
Lin Jen-Shin <godfat@godfat.org>
2013-06-25 22:09:25 +0800

    more tests

--- test/test_any_instance_of.rb
+++ test/test_any_instance_of.rb
@@ -16,15 +16,17 @@ describe Muack::AnyInstanceOf do
     klass = Class.new{ def f; true; end }
     any_instance_of(klass){ |instance| mock_proxy(instance).f }
     obj = klass.new
-    obj.f.should.eq true
+    obj.f       .should.eq true
     Muack.verify.should.eq true
+    obj.f       .should.eq true
   end
 
   should 'proxy any_instance_of with a block' do
     klass = Class.new{ def f; 0; end }
     any_instance_of(klass){ |instance| mock_proxy(instance).f{ |i| i+1 } }
     obj = klass.new
-    obj.f.should.eq 1
+    obj.f       .should.eq 1
     Muack.verify.should.eq true
+    obj.f       .should.eq 0
   end
 end
