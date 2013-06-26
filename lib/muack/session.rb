1e92d171c499c9b46dfc692524f7cca558350184
Lin Jen-Shin <godfat@godfat.org>
2013-06-27 01:35:50 +0800

    shorter

--- lib/muack/session.rb
+++ lib/muack/session.rb
@@ -5,11 +5,11 @@ require 'muack/any_instance_of'
 
 module Muack
   class Session < Hash
-    def mock       obj; self["mk #{obj.__id__}"] ||= Mock     .new(obj); end
-    def stub       obj; self["sb #{obj.__id__}"] ||= Stub     .new(obj); end
+    def mock obj; self["mk #{obj.__id__}"] ||= Mock.new(obj); end
+    def stub obj; self["sb #{obj.__id__}"] ||= Stub.new(obj); end
 
-    def any_instance_of klass
-      (@any_instance_of ||= {})[klass.__id__] ||= AnyInstanceOf.new(klass)
+    def any_instance_of kls
+      (@others ||= {})["ai #{kls.__id__}"] ||= AnyInstanceOf.new(kls)
     end
 
     def verify
