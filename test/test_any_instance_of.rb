209ead2e57a2928e7eb9f7f3b6c4ffbbe6870f59
Lin Jen-Shin <godfat@godfat.org>
2013-06-27 03:16:42 +0800

    try to make the test faster

--- test/test_any_instance_of.rb
+++ test/test_any_instance_of.rb
@@ -2,8 +2,9 @@
 require 'muack/test'
 
 describe Muack::AnyInstanceOf do
+  klass = Class.new{ def f; 0; end }
+
   should 'mock any_instance_of' do
-    klass = Class.new
     any_instance_of(klass){ |instance| mock(instance).say{ true } }
     obj = klass.new
     obj.say              .should.eq true
@@ -13,16 +14,14 @@ describe Muack::AnyInstanceOf do
   end
 
   should 'proxy any_instance_of' do
-    klass = Class.new{ def f; true; end }
     any_instance_of(klass){ |instance| mock(instance).f.proxy }
     obj = klass.new
-    obj.f       .should.eq true
+    obj.f       .should.eq 0
     Muack.verify.should.eq true
-    obj.f       .should.eq true
+    obj.f       .should.eq 0
   end
 
   should 'proxy any_instance_of with a block' do
-    klass = Class.new{ def f; 0; end }
     any_instance_of(klass){ |instance| mock(instance).f{ |i| i+1 }.proxy }
     obj = klass.new
     obj.f       .should.eq 1
@@ -31,7 +30,6 @@ describe Muack::AnyInstanceOf do
   end
 
   should 'proxy with multiple any_instance_of call' do
-    klass = Class.new{ def f; 0; end }
     any_instance_of(klass){ |instance| mock(instance).f{ |i| i+1 }.proxy }
     any_instance_of(klass){ |instance| mock(instance).f{ |i| i+2 }.proxy }
     obj = klass.new
@@ -42,12 +40,11 @@ describe Muack::AnyInstanceOf do
   end
 
   should 'mock with multiple any_instance_of call' do
-    klass = Class.new{ def f; 0; end }
     any_instance_of(klass){ |inst| mock(inst).f(is_a(Fixnum)){ |i| i+1 } }
     any_instance_of(klass){ |inst| mock(inst).f(is_a(Fixnum)){ |i| i+2 } }
     obj = klass.new
-    obj.f(0).should.eq 1
-    obj.f(0).should.eq 2
+    obj.f(2).should.eq 3
+    obj.f(2).should.eq 4
     Muack.verify.should.eq true
     obj.f.should.eq 0
   end
