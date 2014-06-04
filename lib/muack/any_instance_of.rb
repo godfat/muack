8672599029a536acec22dd096a433e367ade9f09
Lin Jen-Shin <godfat@godfat.org>
2014-06-04 16:25:16 +0800

    add dummies for AnyInstanceOf so that a hash could contain everything
    for a session, therefore there's even fewer reasons to make session a
    hash. i should have done this long ago. thanks for
    http://tenderlovemaking.com/2014/06/02/yagni-methods-are-killing-me.html
    to encourage me do this.

--- lib/muack/any_instance_of.rb
+++ lib/muack/any_instance_of.rb
@@ -4,5 +4,9 @@ module Muack
     def inspect
       "Muack::API.any_instance_of(#{singleton_class})"
     end
+
+    # dummies for Muack::Session
+    def __mock_verify; true; end
+    def __mock_reset ;     ; end
   end
 end
--- lib/muack/session.rb
+++ lib/muack/session.rb
@@ -5,20 +5,23 @@ require 'muack/spy'
 require 'muack/any_instance_of'
 
 module Muack
-  class Session < Hash
-    def mock obj; self["mk #{obj.__id__}"] ||= Mock.new(obj)      ; end
-    def stub obj; self["sb #{obj.__id__}"] ||= Stub.new(obj)      ; end
-    def spy  obj; self["sy #{obj.__id__}"] ||= Spy .new(stub(obj)); end
+  class Session
+    attr_reader :data
+    def initialize; @data = {}; end
+
+    def mock obj; data["mk #{obj.__id__}"] ||= Mock.new(obj)      ; end
+    def stub obj; data["sb #{obj.__id__}"] ||= Stub.new(obj)      ; end
+    def spy  obj; data["sy #{obj.__id__}"] ||= Spy .new(stub(obj)); end
 
     def any_instance_of kls
-      (@others ||= {})["ai #{kls.__id__}"] ||= AnyInstanceOf.new(kls)
+      data["ai #{kls.__id__}"] ||= AnyInstanceOf.new(kls)
     end
 
     def verify obj=nil
       if obj
         with(obj, :[]).all?(&:__mock_verify)
       else
-        each_value.all?(&:__mock_verify)
+        data.each_value.all?(&:__mock_verify)
       end
     end
 
@@ -26,15 +29,14 @@ module Muack
       if obj
         with(obj, :delete).each(&:__mock_reset)
       else
-        instance_variable_defined?(:@others) && @others.clear
-        reverse_each{ |_, m| m.__mock_reset }
-        clear
+        data.reverse_each{ |_, m| m.__mock_reset } # reverse_each_value?
+        data.clear
       end
     end
 
     private
     def with obj, meth
-      %w[mk sb sy].map{ |k| __send__(meth, "#{k} #{obj.__id__}") }.compact
+      %w[mk sb sy ai].map{ |k| data.send(meth, "#{k} #{obj.__id__}") }.compact
     end
   end
 end
