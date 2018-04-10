6a23762a19c413a00d0e49d796f7c4aee7e59d1b
Lin Jen-Shin <godfat@godfat.org>
2018-04-11 03:03:52 +0800

    Retain visibility and add tests for it

--- lib/muack/mock.rb
+++ lib/muack/mock.rb
@@ -127,7 +127,8 @@ module Muack
       object.singleton_class.ancestors.first.module_eval do
         remove_method(defi.msg)
         # restore original method
-        if instance_methods(false).include?(defi.original_method) ||
+        if public_instance_methods(false).include?(defi.original_method) ||
+           protected_instance_methods(false).include?(defi.original_method) ||
            private_instance_methods(false).include?(defi.original_method)
           alias_method(defi.msg, defi.original_method)
           __send__(defi.visibility, defi.msg)
@@ -137,8 +138,10 @@ module Muack
     end
 
     def self.store_original_method klass, defi
-      visibility = if klass.instance_methods(false).include?(defi.msg)
-        :public # TODO: forget about protected methods?
+      visibility = if klass.public_instance_methods(false).include?(defi.msg)
+        :public
+      elsif klass.protected_instance_methods(false).include?(defi.msg)
+        :protected
       elsif klass.private_instance_methods(false).include?(defi.msg)
         :private
       end
--- test/test_visibility.rb
+++ test/test_visibility.rb
@@ -1,1 +1,99 @@
-# TODO
+
+require 'muack/test'
+
+describe 'retain visibility' do
+  after do
+    expect(Muack.verify).eq true
+  end
+
+  def verify obj, visibility
+    expect(Muack.verify).eq true
+
+    if visibility == :public
+      expect(obj).respond_to? :hello
+    else
+      expect(obj).not.respond_to? :hello
+      expect(obj).respond_to? :hello, true
+    end
+  end
+
+  def generate visibility
+    klass = Class.new do
+      def greet
+        'hi'
+      end
+    end
+
+    mod = Module.new do
+      def greet
+        hello
+      end
+
+      def hello
+        'hello'
+      end
+      send visibility, :hello
+    end
+
+    perform(klass, mod)
+  end
+
+  copy :test do
+    %i[public protected private].each do |visibility|
+      would "stub with #{visibility}" do
+        obj = generate(visibility)
+
+        stub(obj).hello{'stubbed'}
+
+        verify(obj, visibility)
+      end
+
+      would "stub proxy with #{visibility}" do
+        obj = generate(visibility)
+
+        stub(obj).hello
+
+        verify(obj, visibility)
+      end
+    end
+  end
+
+  describe 'prepend on class stub with object' do
+    def perform(klass, mod)
+      klass.prepend(mod)
+      klass.new
+    end
+
+    paste :test
+  end
+
+  describe 'extend on object stub with object' do
+    def perform(klass, mod)
+      obj = klass.new
+      obj.extend(mod)
+      obj
+    end
+
+    paste :test
+  end
+
+  describe 'include on singleton_class stub with object' do
+    def perform(klass, mod)
+      obj = klass.new
+      obj.singleton_class.include(mod)
+      obj
+    end
+
+    paste :test
+  end
+
+  describe 'prepend on singleton_class stub with object' do
+    def perform(klass, mod)
+      obj = klass.new
+      obj.singleton_class.prepend(mod)
+      obj
+    end
+
+    paste :test
+  end
+end
