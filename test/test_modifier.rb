3bf1b506c1800fd3a0fd1ee76cfdd682a48f5859
Lin Jen-Shin <godfat@godfat.org>
2018-05-29 21:55:42 +0800

    Fix tests for frozen string literals

--- test/test_modifier.rb
+++ test/test_modifier.rb
@@ -37,19 +37,19 @@ describe Muack::Modifier do
 
   describe 'peek_args' do
     would 'with lexical scope' do
-      str = 'ff'
+      str = String.new('ff')
       stub(str).to_i.peek_args{16}
       str.to_i.should.eq 255
     end
 
     would 'with dynamic scope' do
-      str = '16'
+      str = String.new('16')
       stub(str).to_i.peek_args(:instance_exec => true){Integer(self)}
       str.to_i.should.eq 22
     end
 
     would 'modify' do
-      str = 'ff'
+      str = String.new('ff')
       stub(str).to_i(is_a(Integer)).peek_args{ |radix| radix * 2 }
       str.to_i(8).should.eq 255
     end
@@ -82,19 +82,19 @@ describe Muack::Modifier do
 
   describe 'peek_return' do
     would 'with lexical scope' do
-      str = 'ff'
+      str = String.new('ff')
       stub(str).to_i.peek_return{16}
       str.to_i.should.eq 16
     end
 
     would 'with dynamic scope' do
-      str = '16'
+      str = String.new('16')
       stub(str).to_i.peek_return(:instance_exec => true){Integer(self)+1}
       str.to_i.should.eq 17
     end
 
     would 'modify' do
-      str = 'ff'
+      str = String.new('ff')
       stub(str).to_i(is_a(Integer)).peek_return{ |result| result * 2 }
       str.to_i(16).should.eq 510
     end
